a0f18f3d54e19c329527be3551b0efed0e24863b
Renzo Lucioni <renzo@renzolucioni.com>
2016-04-01 00:22:10 -0400

    Add all missing notebooks
    Also add script used to scrape notebooks.

---
+++ scrape.py
@@ -0,0 +1,76 @@
+import logging
+import os
+import time
+
+from bs4 import BeautifulSoup
+import requests
+from slugify import slugify
+
+
+logging.basicConfig(
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO
+)
+
+readme = 'http://norvig.com/ipython/README.html'
+extension = '.ipynb'
+
+
+def main():
+    logging.info('Retrieving notebooks from [%s].', readme)
+
+    soup = soupify(readme)
+
+    search(soup)
+
+
+def soupify(url):
+    response = requests.get(readme)
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    return soup
+
+
+def search(soup):
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if not href.startswith('http://'):
+            sync(href)
+
+
+def sync(notebook):
+    logging.info('Syncing notebook [%s].', notebook)
+
+    slug = slugify(notebook.split(extension)[0])
+    path = 'notebooks/{slug}{extension}'.format(slug=slug, extension=extension)
+
+    if os.path.isfile(path):
+        logging.info('Notebook [%s] exists at [%s].', notebook, path)
+    else:
+        logging.info('Saving new notebook [%s].', notebook)
+        save(notebook, path)
+
+
+
+def save(notebook, path):
+    start = now()
+
+    response = requests.get('http://norvig.com/ipython/' + notebook)
+    raw = response.text
+
+    with open(path, 'w') as f:
+        f.write(raw)
+
+    end = now()
+    elapsed = end - start
+    logging.info('Saved notebook [%s] to [%s] in [%.2f] seconds.', notebook, path, elapsed)
+
+
+def now():
+    return time.time()
+
+
+if __name__ == '__main__':
+    main()
